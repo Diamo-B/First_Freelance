eec3e6710acadf785d0b64bb37f1083a98ab5ffe
Bachar <diamobachar06@gmail.com>
2022-12-26 21:38:21 +0100

    made a custom hook to determine Desktop/mobile modes made a new desktop footer and applied the responsiveness made a new general layout navbar for desktop and applied the responsiveness removed some npm packages I didn't use

--- components/Footer.js
+++ components/Footer.js
@@ -1,24 +1,38 @@
 import Image from "next/image";
+import Link from "next/link";
+import useBetterMediaQuery from "/components/useBetterMediaQuery";
 
 const Footer = () => {
+    const isMobile = useBetterMediaQuery('(max-width: 500px)');
     return ( 
         <footer>
-            <div className="Footerlogo">
-                <Image src="/logo.svg" alt="logo" width={38.8} height={40}/>
-                <p>LoremIpsum.com</p>
+            <div className="DesktopFooter">
+                <div className="Footerlogo">
+                    <Image src="/logo.svg" alt="logo" width={38.8} height={40}/>
+                    <p>LoremIpsum.com</p>
+                </div>
+                {
+                    ! isMobile &&
+                    <div className="categories">
+                        <Link href='/Categories/Electroniques'>Electroniques</Link>
+                        <Link href='/Categories/Cosmétiques'>Cosmétiques</Link>
+                        <Link href='/Categories/Cuisine'>Cuisine</Link>
+                        <Link href='/Categories/Divers'>Divers</Link>
+                    </div>
+                } 
+                <div className="Sm-wrapper">
+                    <p className="text">
+                    Veuillez suivez nos pages sur les platformes du social media pour toutes sortes d&apos;actualités
+                    </p>
+                    <div className="social-media">
+                        <a href=""><Image src="/facebook.svg" width={25} height={25} alt="facebook"/></a>
+                        <a href=""><Image src="/instagram.svg" width={25} height={25} alt="instagram"/></a>
+                        <a href=""><Image src="/whatsapp.svg" width={25} height={25} alt="whatsapp"/></a>
+                        <a href=""><Image src="/tiktok.svg" width={25} height={25} alt="tiktok"/></a>
+                    </div>
+                </div>
             </div>
-            <p className="text">
-            Veuillez suivez nos pages sur les platformes du social media pour toutes sortes d&apos;actualités en cliquant sur une des boutons ci-dessous
-            </p>
-            <div className="social-media">
-                <a href=""><Image src="/facebook.svg" width={25} height={25} alt="facebook"/></a>
-                <a href=""><Image src="/instagram.svg" width={25} height={25} alt="instagram"/></a>
-                <a href=""><Image src="/whatsapp.svg" width={25} height={25} alt="whatsapp"/></a>
-                <a href=""><Image src="/tiktok.svg" width={25} height={25} alt="tiktok"/></a>
-            </div>
-            <p className="Copyright">
-            Copyright 2022 .All rights Reserved
-            </p>
+            <p className="Copyright">Copyright 2022 .All rights Reserved</p>
         </footer>
     );
 }
--- components/Layouts/GeneralLayout.js
+++ components/Layouts/GeneralLayout.js
@@ -1,13 +1,16 @@
 import Navbar from "../navbars/Navbar";
+import BrowserNavGen from "/components/navbars/browser/Navbar";
 import Footer from '../Footer';
 import { useState, useEffect } from "react";
 import Menu from "../Menu";
 import Head from "next/head";
+import useBetterMediaQuery from "/components/useBetterMediaQuery";
 
 const Layout = ({children}) => {
   const [isActive, setIsActive] = useState(false);
   let [searchData,setSearchData] = useState(null);
 
+  const isMobile = useBetterMediaQuery('(max-width: 500px)');
   useEffect(() => {
     fetch("/api/products/getProducts/getAllProducts",{
         method:"GET",
@@ -20,15 +23,24 @@ const Layout = ({children}) => {
       setSearchData(data)
     })
   }, []);
-
   return ( 
     <>
       <Head>
         <title>LoremIpsum.com</title>
       </Head> 
       <div className="body">
-        <Menu isActive={isActive} setIsActive={setIsActive}/>
-        <Navbar isActive={isActive} setIsActive={setIsActive} searchData={searchData}/>
+        {
+          isMobile?
+          <>
+            <Menu isActive={isActive} setIsActive={setIsActive}/>
+            <Navbar isActive={isActive} setIsActive={setIsActive} searchData={searchData}/>
+          </>
+          :
+          <>
+            <BrowserNavGen searchData={searchData}/>
+          </>
+
+        }
         <main>
           {children}
         </main>
